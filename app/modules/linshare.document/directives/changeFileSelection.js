90a868fdea2642f555d064c7be35ef9eed282b74
Alpha Sall <asall@linagora.com>
2016-05-09 16:50:14 +0200

    Fix #1169 - Get document thumbnail and shares recipients on file selection Keep the info action buttons, highlighted on the last clicked element Code refactoring for directive dealing with a document selection

--- app/modules/linshare.document/directives/changeFileSelection.js
+++ app/modules/linshare.document/directives/changeFileSelection.js
@@ -1,118 +1,70 @@
 'use strict';
 
 angular.module('linshare.document')
-  .directive('fileSelection', function() {
-    return {
-      restrict: 'A',
-      scope: {
-        fileSelection: '=',
-        uuid: '='
-      },
-      link: function(scope, element) {
-        element.on('change', function(){
-          element.parent().parent().toggleClass('info');
-          if(element.is(':checked')) {
-            scope.$apply(function() {
-              scope.fileSelection.push(scope.uuid);
-            });
 
+  // DIRECTIVE TO TOGGLE DOCUMENT SELECTION
+  .directive('lsOnDocumentSelect', function(LinshareDocumentService, $log) {
+    var toggleDocumentSelection = function(scope) {
+      scope.currentSelectedDocument.current = scope.documentFile;
+      scope.documentFile['isSelected'] = !scope.documentFile['isSelected'];
+      if(scope.documentFile.isSelected) {
+        if(scope.sidebarRight) {
+          if(scope.documentFile.shared > 0) {
+            LinshareDocumentService.getFileInfo(scope.documentFile.uuid).then(function(data) {
+              scope.currentSelectedDocument.current.shares = data.shares;
+            });
           }
-          else {
-            var index = scope.fileSelection.indexOf(scope.uuid);
-            if (index > -1) {
-              scope.$apply(function() {
-                scope.fileSelection.splice(index, 1);
-              });
-            }
+          if(scope.documentFile.hasThumbnail === true) {
+            LinshareDocumentService.getThumbnail(scope.documentFile.uuid).then(function(thumbnail) {
+              scope.currentSelectedDocument.current.thumbnail = thumbnail;
+            });
           }
+        }
+        scope.$apply(function() {
+          scope.selectedDocuments.push(scope.documentFile);
         });
+      } else {
+        var indexMulSelect = scope.selectedDocuments.indexOf(scope.documentFile);
+        if(indexMulSelect > -1) {
+          scope.$apply(function() {
+            scope.selectedDocuments.splice(indexMulSelect, 1);
+          });
+        }
       }
     };
-  })
-  .directive('lsOnDocumentSelect', function(LinshareDocumentService) {
+
     return {
       restrict: 'A',
       scope: {
-        fileSelectionV: '=',
+        selectedDocuments: '=fileSelectionV',
         documentFile: '=uuid',
         currentSelectedDocument: '=',
-        multipleSelectOn: '='
+        sidebarRight: '='
       },
       link: function(scope, element, attr) {
         element.bind('contextmenu', function(event) {
-        var classes =  angular.element(this).attr('class');
-          var isHighlighted = angular.element(this).hasClass("highlightListElem");
-          if(!isHighlighted){
-            setSelected();
+          var isHighlighted = element.hasClass('highlightListElem');
+          if(!isHighlighted) {
+            toggleDocumentSelection(scope);
           }
         });
         element.bind('click', function() {
-          setSelected();
-        });
-        function setSelected(){
-          if(scope.multipleSelectOn === true) {
-            element.toggleClass('highlightListElem');
-            if(element.hasClass('highlightListElem')) {
-              scope.documentFile['isSelected'] = true;
-              scope.$apply(function() {
-                scope.currentSelectedDocument.current = scope.documentFile;
-                scope.fileSelectionV.push(scope.documentFile);
-              });
-            } else {
-              scope.documentFile['isSelected'] = false;
-              var indexMulSelect = scope.fileSelectionV.indexOf(scope.documentFile);
-              if (indexMulSelect > -1) {
-                scope.$apply(function() {
-                  scope.fileSelectionV.splice(indexMulSelect, 1);
-                  scope.currentSelectedDocument.current = scope.fileSelectionV[scope.fileSelectionV.length -1];
-                });
-              }
-            }
-          } else {
-            element.siblings('.info').removeClass('info');
-            if(scope.fileSelectionV.length > 1) {
-              element.addClass('info');
-              scope.$apply(function() {
-                scope.fileSelectionV.splice(0, scope.fileSelectionV.length);
-                scope.fileSelectionV.push(scope.documentFile);
-              });
-            } else {
-              element.toggleClass('info');
-              if(element.hasClass('info')) {
-                if(scope.documentFile.hasThumbnail === true && attr.sidebar === 'true') {
-                  LinshareDocumentService.getThumbnail(scope.documentFile.uuid).then(function(thumbnail) {
-                    scope.documentFile.thumbnail = thumbnail;
-                  });
-                }
-                if(scope.documentFile.shared > 0) {
-                  LinshareDocumentService.getFileInfo(scope.documentFile.uuid).then(function(data) {
-                    scope.documentFile.shares = data.shares;
-                  });
-                }
-                scope.$apply(function() {
-                  scope.fileSelectionV.shift();
-                  scope.fileSelectionV.push(scope.documentFile);
-                });
-              } else {
-                var indexOneSelect = scope.fileSelectionV.indexOf(scope.documentFile);
-                if (indexOneSelect > -1) {
-                  scope.$apply(function() {
-                    scope.fileSelectionV.splice(indexOneSelect, 1);
-                  });
-                }
-              }
-            }
+          if(scope.sidebarRight) {
+            element.siblings().find('li.activeActionButton').removeClass('activeActionButton');
+            element.find('li')[0].className = 'activeActionButton';
           }
-        }
+          toggleDocumentSelection(scope);
+        });
       }
-
     };
   })
-  .directive('sidebarContent', function($parse) {
+
+  //DIRECTIVE TO DISPLAY DIFFERENT CONTENTS INSIDE THE RIGHT SIDEBAR
+  .directive('sidebarContent', function() {
     return {
       restrict: 'A',
       templateUrl: function(elm, attr) {
-        return 'modules/linshare.document/directives/sidebarContent-'+attr.sidebarContent+'.html';
+        return 'modules/linshare.document/directives/sidebarContent-' + attr.sidebarContent + '.html';
       }
     }
   });
