1c5a0d7815f60fc856bb7a83ab859d68023af2e4
Sarra Mamlouk <smamlouk@linagora.com>
2016-07-08 14:20:23 +0200

    Feature: workgroup file details sidebar

--- app/scripts/sharedspace/SharedSpaceListController.js
+++ app/scripts/sharedspace/SharedSpaceListController.js
@@ -5,4 +5,25 @@ angular.module('linshareUiUserApp')
     $scope.$on('$stateChangeSuccess', function () {
       angular.element('.multi-select-mobile').appendTo('body');
     });
+
+    $scope.loadSidebarContent = function(content) {
+      $scope.sidebarRightDataType = content;
+    };
+    $scope.slideTextarea = function($event){
+      var currTarget = $event.currentTarget;
+      angular.element(currTarget).parent().addClass('show-full-comment');
+      };
+    $scope.slideUpTextarea = function($event){
+      var currTarget = $event.currentTarget;
+      angular.element(currTarget).parent().removeClass('show-full-comment');
+    };
+    $scope.setTextInput = function ($event) {
+      var currTarget = $event.currentTarget;
+      var inputTxt = angular.element(currTarget).text();
+      if (inputTxt === '') {
+        angular.element(currTarget).parent().find('span').css('display', 'block');
+      } else {
+        angular.element(currTarget).parent().find('span').css('display', 'none');
+      }
+    };
   });
