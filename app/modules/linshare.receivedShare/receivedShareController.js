eae9028ac7b0e3cec6a10ea1dee858dea475c365
Kévin LEMELE <klemele@linagora.com>
2017-03-22 18:58:42 +0100

    Remove unused code

--- app/modules/linshare.receivedShare/receivedShareController.js
+++ app/modules/linshare.receivedShare/receivedShareController.js
@@ -540,29 +540,6 @@ angular.module('linshare.receivedShare')
         angular.element('.multi-select-mobile').appendTo('body');
       });
 
-      $scope.fab = {
-        isOpen: false,
-        count: 0,
-        selectedDirection: 'left'
-      };
-
-      $scope.$watch('fab.isOpen', function(isOpen) {
-        if (isOpen) {
-          angular.element('.md-toolbar-tools').addClass('setWhite');
-          angular.element('.multi-select-mobile').addClass('setDisabled');
-          $timeout(function() {
-            angular.element('#overlayMobileFab').addClass('toggledMobileShowOverlay');
-            angular.element('#content-container').addClass('setDisabled');
-          }, 250);
-        } else {
-          angular.element('.md-toolbar-tools').removeClass('setWhite');
-          $timeout(function() {
-            angular.element('.multi-select-mobile').removeClass('setDisabled');
-            angular.element('#overlayMobileFab').removeClass('toggledMobileShowOverlay');
-            angular.element('#content-container').removeClass('setDisabled');
-          }, 250);
-        }
-      });
       $scope.currentPage = 'received_files';
       $scope.lsFormat = function() {
         return $translate.use() === 'fr-FR' ? 'd MMMM y' : 'MMMM d y';
