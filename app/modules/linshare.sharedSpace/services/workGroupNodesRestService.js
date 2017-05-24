424b61db4d74327e215c165f06a23335375dec93
Ismail ANWAR-BAIG <ianwar@linagora.com>
2017-05-24 16:22:45 +0200

    Workgroup sidebar details - lastAuthor details implemented

--- app/modules/linshare.sharedSpace/services/workGroupNodesRestService.js
+++ app/modules/linshare.sharedSpace/services/workGroupNodesRestService.js
@@ -12,9 +12,9 @@
   workgroupNodesRestService.$inject = ['_', '$log', 'Restangular', 'ServerManagerService'];
 
   /**
-   *  @namespace workgroupNodesRestService
-   *  @desc Service to interact with Workgroup Nodes object by REST
-   *  @memberOf LinShare.sharedSpace
+   * @namespace workgroupNodesRestService
+   * @desc Service to interact with Workgroup Nodes object by REST
+   * @memberOf LinShare.sharedSpace
    */
   function workgroupNodesRestService(_, $log, Restangular, ServerManagerService) {
     var
@@ -35,34 +35,56 @@
         update: update
       };
 
+    activate();
+
     return service;
 
     ////////////
 
     /**
-     *  @name copy
-     *  @desc copy a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {object} nodeItem - Node to duplicate
-     *  @param {string} destinationNodeUuid - The uuid of the Destination Node object
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name activate
+     * @desc Activation function of the controller, launch at every instantiation
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     */
+    function activate() {
+      Restangular.extendModel(restParam, function(model) {
+        if (_.isObject(model)) {
+          model.getName = function() {
+            if (this.lastAuthor.firstName) {
+              return this.lastAuthor.firstName + ' ' + this.lastAuthor.lastName;
+            } else {
+              return this.lastAuthor.mail;
+            }
+          };
+        }
+        return model;
+      });
+    }
+
+    /**
+     * @name copy
+     * @desc copy a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {object} nodeItem - Node to duplicate
+     * @param {string} destinationNodeUuid - The uuid of the Destination Node object
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function copy(workgroupUuid, nodeItem, destinationNodeUuid) {
       $log.debug('workgroupNodesRestService : copy', workgroupUuid, nodeItem.uuid, destinationNodeUuid);
       var _destinationNodeUuid = _.isNil(destinationNodeUuid) ? '' : destinationNodeUuid;
       return handler(Restangular.one(restUrl, workgroupUuid).one(restParam, nodeItem.uuid)
-        .all('copy').post(nodeItem, {destinationNodeUuid:  _destinationNodeUuid}), errorsMessagesKey);
+        .all('copy').post(nodeItem, {destinationNodeUuid: _destinationNodeUuid}), errorsMessagesKey);
     }
 
     /**
-     *  @name create
-     *  @desc Create a Workgroup Node object
-     *  @param {string} workgroupUuid - The id of a Workgroup object
-     *  @param {object} workgroupFolderDto - The Workgroup Folder object
-     *  @param {boolean} dryRun - If true, the server returns a fake Node object with unique name
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name create
+     * @desc Create a Workgroup Node object
+     * @param {string} workgroupUuid - The id of a Workgroup object
+     * @param {object} workgroupFolderDto - The Workgroup Folder object
+     * @param {boolean} dryRun - If true, the server returns a fake Node object with unique name
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function create(workgroupUuid, workgroupFolderDto, dryRun) {
       $log.debug('workgroupNodesRestService : create', workgroupUuid, workgroupFolderDto, dryRun);
@@ -70,12 +92,12 @@
     }
 
     /**
-     *  @name download
-     *  @desc Download a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} nodeUuid - The uuid of the Workgroup Node object
-     *  @returns {string} direct download url of the ressource
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name download
+     * @desc Download a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} nodeUuid - The uuid of the Workgroup Node object
+     * @returns {string} direct download url of the ressource
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function download(workgroupUuid, nodeUuid) {
       $log.debug('workgroupNodesRestService : download', workgroupUuid, nodeUuid);
@@ -83,13 +105,13 @@
     }
 
     /**
-     *  @name get
-     *  @desc Get a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} nodeUuid - The uuid of the Workgroup Node object
-     *  @param {boolean} needTree - If true, add the ancestors of the related folder
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name get
+     * @desc Get a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} nodeUuid - The uuid of the Workgroup Node object
+     * @param {boolean} needTree - If true, add the ancestors of the related folder
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function get(workgroupUuid, nodeUuid, needTree) {
       $log.debug('workgroupNodesRestService : get', workgroupUuid, nodeUuid);
@@ -97,12 +119,12 @@
     }
 
     /**
-     *  @name getAudit
-     *  @desc Get audit of a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} nodeUuid - The uuid of the Workgroup Node object
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name getAudit
+     * @desc Get audit of a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} nodeUuid - The uuid of the Workgroup Node object
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function getAudit(workgroupUuid, nodeUuid) {
       $log.debug('workgroupNodesRestService : getAudit', workgroupUuid, nodeUuid);
@@ -110,13 +132,13 @@
     }
 
     /**
-     *  @name getList
-     *  @desc Get the List of Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} folderUuid - The uuid of a Workgroup Folder object
-     *  @param {string} nodeType - Type of nodes to get
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name getList
+     * @desc Get the List of Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} folderUuid - The uuid of a Workgroup Folder object
+     * @param {string} nodeType - Type of nodes to get
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function getList(workgroupUuid, folderUuid, nodeType) {
       $log.debug('workgroupNodesRestService : getList', workgroupUuid, folderUuid);
@@ -127,12 +149,12 @@
     }
 
     /**
-     *  @name remove
-     *  @desc Remove a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} nodeUuid - The uuid of the Workgroup Node object
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name remove
+     * @desc Remove a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} nodeUuid - The uuid of the Workgroup Node object
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function remove(workgroupUuid, nodeUuid) {
       $log.debug('workgroupNodesRestService : remove', workgroupUuid, nodeUuid);
@@ -153,12 +175,12 @@
     }
 
     /**
-     *  @name thumbnail
-     *  @desc Get the file thumbnail of a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {string} nodeUuid - The uuid of the Workgroup Node object
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name thumbnail
+     * @desc Get the file thumbnail of a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {string} nodeUuid - The uuid of the Workgroup Node object
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function thumbnail(workgroupUuid, nodeUuid) {
       $log.debug('workgroupNodesRestService : thumbnail', workgroupUuid, nodeUuid);
@@ -168,12 +190,12 @@
     }
 
     /**
-     *  @name update
-     *  @desc Update a Workgroup Node object
-     *  @param {string} workgroupUuid - The uuid of the Workgroup object
-     *  @param {object} nodeItem - Node to update
-     *  @returns {Promise} server response
-     *  @memberOf LinShare.sharedSpace.workgroupNodesRestService
+     * @name update
+     * @desc Update a Workgroup Node object
+     * @param {string} workgroupUuid - The uuid of the Workgroup object
+     * @param {object} nodeItem - Node to update
+     * @returns {Promise} server response
+     * @memberOf LinShare.sharedSpace.workgroupNodesRestService
      */
     function update(workgroupUuid, nodeItem) {
       $log.debug('workgroupNodesRestService : update', workgroupUuid, nodeItem.uuid, nodeItem);
