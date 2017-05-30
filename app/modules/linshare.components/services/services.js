7d914e9eabc46a3b97a1272480ce060bdf2fe5d5
Kévin LEMELE <klemele@linagora.com>
2017-05-30 16:58:40 +0200

    Remove dead code

--- app/modules/linshare.components/services/services.js
+++ app/modules/linshare.components/services/services.js
@@ -14,7 +14,7 @@ angular.module('linshare.components')
       $(selector).mCustomScrollbar({
         theme: theme,
         scrollInertia: 100,
-        axis:'yx',
+        axis: 'yx',
         mouseWheel: {
           enable: true,
           axis: mousewheelaxis,
@@ -24,74 +24,4 @@ angular.module('linshare.components')
     };
 
     return ss;
-  })
-
-  //==============================================
-  // BOOTSTRAP GROWL
-  //==============================================
-
-  .service('growlService', function($translate) {
-    var fromTopAlignRight = {from: 'top', align: 'right', icon: 'icon', timer: 1000};
-    var fromBottomAlignLeft = {from: 'bottom', align: 'left', icon: 'icon', timer: 1000};
-    var fromBottomAlignRight = {from: 'bottom', align: 'right', icon: 'icon', timer: 1000};
-    var fromTopCenter = {from: 'top', align: 'center', icon: 'icon', timer: 0};
-    // option = {from, align, icon, type, animIn, animOut}
-    var growlNotifyer = function(message, type, option) {
-      $translate([message]).then(function(translation) {
-        $.growl({
-          icon: option.icon,
-          title: '',
-          message: translation[message],
-          url: ''
-        }, {
-          element: 'body',
-          type: type,
-          allow_dismiss: true,
-          placement: {
-            from: option.from,
-            align: option.align
-          },
-          offset: {
-            x: 20,
-            y: 85
-          },
-          spacing: 10,
-          z_index: 1031,
-          delay: 2500,
-          timer: option.timer,
-          url_target: '_blank',
-          mouse_over: false,
-          animate: {
-            enter: option.animIn,
-            exit: option.animOut
-          },
-          icon_type: 'class',
-          template: '<div data-growl="container" class="alert" role="alert">' +
-          '<button type="button" class="close" data-growl="dismiss">' +
-          '<span aria-hidden="true">&times;</span>' +
-          '<span class="sr-only">Close</span>' +
-          '</button>' +
-          '<span data-growl="icon"></span>' +
-          '<span data-growl="title"></span>' +
-          '<span data-growl="message"></span>' +
-          '<a href="#" data-growl="url"></a>' +
-          '</div>'
-        });
-      });
-    };
-    return {
-      notifyer: growlNotifyer,
-      notifyTopRight: function(message, type) {
-        return growlNotifyer(message, type, fromTopAlignRight);
-      },
-      notifyBottomLeft: function(message, type) {
-        return growlNotifyer(message, type, fromBottomAlignLeft);
-      },
-      notifyBottomRight: function(message, type) {
-        return growlNotifyer(message, type, fromBottomAlignRight);
-      },
-      notifyTopCenter: function(message, type) {
-        return growlNotifyer(message, type, fromTopCenter);
-      }
-    };
   });
