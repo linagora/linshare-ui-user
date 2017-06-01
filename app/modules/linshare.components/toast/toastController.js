aec4567bc96f9b7adc666ef96a8c295cb6ced0c4
Kévin LEMELE <klemele@linagora.com>
2017-06-01 15:19:06 +0200

    Fix! ToastService - Added a listener to remove remaining Toast in the DOM when the service $mdToast fail to do it properly

--- app/modules/linshare.components/toast/toastController.js
+++ app/modules/linshare.components/toast/toastController.js
@@ -9,17 +9,33 @@
     .module('linshare.components')
     .controller('toastController', toastController);
 
-  toastController.$inject = ['_'];
+  toastController.$inject = ['_', '$scope'];
 
   /**
    * @namespace toastController
    * @desc Controller for the mdToast
    * @memberOf linshare.components.toastController
    */
-  function toastController(_) {
+  function toastController(_, $scope) {
     /* jshint validthis:true */
     var toastVm = this;
-    toastVm._ = _;
-    _.extend(toastVm.scope, toastVm.locals);
+
+    activate();
+
+    ////////////////
+
+    /**
+     * @namespace activate
+     * @desc Activation function of the controller launch at every instantiation
+     * @memberOf LinShare.components.toastController
+     */
+    function activate() {
+      toastVm._ = _;
+      _.extend(toastVm.scope, toastVm.locals);
+      $scope.$on('$destroy', function() {
+        /* Ensure that an unbind toast didn't correctly disapear */
+        angular.element('md-toast').remove();
+      });
+    }
   }
 })();
