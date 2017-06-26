f95640e014bc840627f55b37a5bfa65eb615fdbb
Kévin LEMELE <klemele@linagora.com>
2017-06-26 16:23:58 +0200

    $q.allSettled implementation
     - Implementation of allSettled function from Kris Kowal's Q: https://github.com/kriskowal/q/wiki/API-Reference#promiseallsettled

--- app/scripts/run.js
+++ app/scripts/run.js
@@ -2,6 +2,37 @@
 
 angular
   .module('linshareUiUserApp')
+  .config(['$provide', function ($provide) {
+    $provide.decorator('$q', ['$delegate', function ($delegate) {
+      var $q = $delegate;
+
+      $q.allSettled = $q.allSettled || function allSettled(promises) {
+        // Implementation of allSettled function from Kris Kowal's Q:
+        // https://github.com/kriskowal/q/wiki/API-Reference#promiseallsettled
+
+        var wrapped = angular.isArray(promises) ? [] : {};
+
+        angular.forEach(promises, function(promise, key) {
+          if (!wrapped.hasOwnProperty(key)) {
+            wrapped[key] = wrap(promise);
+          }
+        });
+
+        return $q.all(wrapped);
+
+        function wrap(promise) {
+          return $q.when(promise)
+            .then(function (value) {
+              return { state: 'fulfilled', value: value };
+            }, function (reason) {
+              return { state: 'rejected', reason: reason };
+            });
+        }
+      };
+
+      return $q;
+    }]);
+  }])
   .config(function(_, RestangularProvider, flowFactoryProvider, $compileProvider, $translateProvider,
                    $translatePartialLoaderProvider, lsAppConfig, lsUserConfig, $windowProvider) {
     lsAppConfig = _.assign(lsAppConfig, lsUserConfig);
