475f747d8ffde1ff989928537ec0361a2e8716b2
Kévin LEMELE <klemele@linagora.com>
2017-10-05 16:11:42 +0200

    Server manager service: add multiple response handler
    - Review response handler
        - Shall be merged into a single function once every rest service are up-to-date
        - Shall update it to manage also success response
        - Shall update every rest service to accept bulk command

--- app/scripts/services/serverManagerService.js
+++ app/scripts/services/serverManagerService.js
@@ -19,6 +19,7 @@
   function ServerManagerService(_, $log, $q, $translate, $translatePartialLoader, toastService) {
     $translatePartialLoader.addPart('serverResponse');
     var service = {
+      multiResponsesHanlder: multiResponsesHanlder,
       responseHandler: responseHandler
     };
 
@@ -27,32 +28,18 @@
     ////////////
 
     /**
-     * @name notify
-     * @desc Send message to the console and by toast
-     * @param {String} message - The main message
-     * @param {String} details - The details message
-     * @memberOf linshareUiUserApp.ServerManagerService
-     */
-    function notify(message, details) {
-      var errorToShow = (_.isUndefined(details) || details === '') ? message : message + ': ' + details;
-      if (!toastService.isActive()) {
-        toastService.error({key: errorToShow});
-      }
-      $log.error(errorToShow);
-    }
-
-    /**
-     * @name responseHandler
-     * @desc Manage server response
-     * @param {Function} action - A function calling the server side
+     * @name errorHandler
+     * @desc Manage message response on a error
+     * @param {Object} error - Server response error
      * @param {String} messagePrefix - The key of errors's type to show
-     * @param {boolean} showError - Determine if the error shall be shown to the user
-     * @returns {Promise} The promise in resolve/reject of the function called
+     * @returns {Promise} The computed error response
      * @memberOf linshareUiUserApp.ServerManagerService
      */
-    function responseHandler(action, messagePrefix, showError) {
-      showError = _.isUndefined(showError) ? true : showError;
-      messagePrefix = _.isUndefined(messagePrefix) ? 'DEFAULT' : messagePrefix;
+    function errorHandler(error, messagePrefix) {
+      var
+        errCode,
+        errorMessageHttpCodes = 'SERVER_RESPONSE.HTTP_CODES.',
+        errorMessageDetails = 'SERVER_RESPONSE.DETAILS.' + messagePrefix + '.';
       const ERRROS_HTTP = {
         400: 'ERROR_400',
         401: 'ERROR_401',
@@ -66,50 +53,144 @@
         503: 'ERROR_503',
         520: 'ERROR_520'
       };
-      var
-        errorData,
-        deferred = $q.defer(),
-        errCode,
-        errorMessageHttpCodes = 'SERVER_RESPONSE.HTTP_CODES.',
-        errorMessageDetails = 'SERVER_RESPONSE.DETAILS.' + messagePrefix + '.';
 
-      action
-        .then(function(data) {
-          deferred.resolve(data);
-        })
-        .catch(function(error) {
-          $log.debug('ServerManagerService - responseHandler:' + error);
-          errorData = error;
-          errCode = _.isNil(errorData.data) ? null : errorData.data.errCode;
-          deferred.reject(error);
+      $log.debug('ServerManagerService - responseHandler:' + error);
 
-          if (ERRROS_HTTP.hasOwnProperty(error.status)) {
-            errorMessageHttpCodes += ERRROS_HTTP[error.status];
-            if (error.status === 404 || error.status === 503) {
-              errCode = errCode || 'SERVER';
-            }
+      errorMessageHttpCodes += ERRROS_HTTP.hasOwnProperty(error.status) ? ERRROS_HTTP[error.status] : 'ERROR_DEFAULT';
+      errCode = _.isNil(error.data) ? null : error.data.errCode;
+      if (error.status === 404 || error.status === 503) {
+        errCode = errCode || 'SERVER';
+      }
+      errorMessageDetails += _.isNil(errCode) ? 'NONE' : errCode;
+      return $translate.refresh().then(function() {
+        return $translate([errorMessageHttpCodes, errorMessageDetails]);
+      }).then(function(translations) {
+        if (!(translations[errorMessageHttpCodes] === errorMessageHttpCodes ||
+              translations[errorMessageDetails] === errorMessageDetails)) {
+          return translations[errorMessageHttpCodes] + ': ' + translations[errorMessageDetails];
+        } else {
+          if (_.isUndefined(errCode)) {
+            return error.statusText;
           } else {
-            errorMessageHttpCodes += 'ERROR_DEFAULT';
+            return error.statusText, errCode + ': ' + error.data.message;
           }
-          if (showError) {
-            errorMessageDetails += _.isNil(errCode) ? 'NONE' : errCode;
-            $translate.refresh().then(function() {
-              $translate([errorMessageHttpCodes, errorMessageDetails]).then(function(translations) {
-                if (!(translations[errorMessageHttpCodes] === errorMessageHttpCodes ||
-                    translations[errorMessageDetails] === errorMessageDetails)) {
-                  notify(translations[errorMessageHttpCodes], translations[errorMessageDetails]);
-                } else {
-                  if (_.isUndefined(errCode)) {
-                    notify(errorData.statusText);
-                  } else {
-                    notify(errorData.statusText, errCode + ': ' + errorData.data.message);
-                  }
-                }
-              });
-            });
+        }
+      });
+    }
+    /**
+     * @name notify
+     * @desc Send message to the console and by toast
+     * @param {boolean} isError - determine if an error toast shall be shown
+     * @param {Object} message - The message object
+     * @param {Object[]} [details] - List of element composing the detailed information
+     * @memberOf linshareUiUserApp.ServerManagerService
+     */
+    function notify(isError, message, details) {
+      isError = _.isNil(isError) ? false : isError;
+      $log.debug('ServerManagerService - notify - message:' + message);
+      $log.debug('ServerManagerService - notify - details:' + details);
+      if (isError) {
+        toastService.error(message, undefined, details);
+      } else {
+        toastService.success(message, undefined, details);
+      }
+    }
+
+    /**
+     * @name multiResponsesHandler
+     * @desc Manage response of multiple promises at once
+     * @param {Object[]} elements - The array of object  with respective promise to handle
+     * @param {Object} elements.object - The object reference
+     * @param {Promise} elements.response - The promise of the object action
+     * @param {Object} translation - Object containing translations information
+     * @param {string} translation.key - Translation key value
+     * @param {string} [translation.messagePrefix] - The key of errors's type to show
+     * @param {Object} [translation.params] - Parameters of the translated sentence
+     * @param {boolean} [translation.pluralization] - Determine if the pluralization interpolation shall be used
+     * @param {boolean} isPrivate - Determine if the error shall be hidden or shown to the user as a toast
+     * @returns {Promise} The array of rejected promise or array of all promises resolved
+     * @memberOf linshareUiUserApp.ServerManagerService
+     */
+    function multiResponsesHanlder(elements, translation, isPrivate) {
+      var
+        successResponses = [],
+        errorResponses = [],
+        objectsReferences = _.map(elements, 'object'),
+        responses = [];
+
+      isPrivate = _.isNil(isPrivate) ? false : isPrivate;
+      translation.messagePrefix = _.isUndefined(translation.messagePrefix) ? 'DEFAULT' : translation.messagePrefix;
+
+      elements = elements || [];
+      if (elements.length === 0 ) {
+        return $q.resolve(elements);
+      }
+
+      return $q.allSettled(_.map(elements, 'response')).then(function(promises) {
+        _.forEach(promises, function(promise) {
+          if (promise.state === 'rejected') {
+            errorResponses.push(promise);
+          } else {
+            successResponses.push(promise);
           }
         });
-      return deferred.promise;
+
+        responses = _.map(errorResponses, function(responseItems) {
+          var objectInError = _.find(objectsReferences, {'uuid': responseItems.reason.config.data.uuid});
+          return errorHandler(responseItems.reason, translation.messagePrefix).then(function(message) {
+            var currentResponse = {
+              title: objectInError.name,
+              message: {
+                key: message
+              }
+            };
+            return currentResponse;
+          });
+        });
+
+        if (errorResponses.length > 0) {
+          return $q.allSettled(responses).then(function(responsesPromises) {
+            var errorTranslation= _.assign(translation , {
+              key: 'TOAST_ALERT.ERROR.' + translation.key + '.DEFAULT',
+              params: {
+                nbItems: errorResponses.length,
+                singular: errorResponses.length === 1
+              }});
+            if (!isPrivate) {
+              notify(true, errorTranslation, _.map(responsesPromises, 'value'));
+            }
+            return $q.reject(errorResponses);
+          });
+        } else {
+          return $q.resolve(successResponses);
+        }
+      });
     }
+
+    /**
+     * @name responseHandler
+     * @desc Manage server response
+     * @param {Function} action - A function calling the server side
+     * @param {String} messagePrefix - The key of errors's type to show
+     * @param {boolean} isPrivate - Determine if the error shall be hidden or shown to the user as a toast
+     * @returns {Promise} The promise in resolve/reject of the function called
+     * @memberOf linshareUiUserApp.ServerManagerService
+     */
+    function responseHandler(action, messagePrefix, isPrivate) {
+      isPrivate = _.isNil(isPrivate) ? false : isPrivate;
+      messagePrefix = _.isUndefined(messagePrefix) ? 'DEFAULT' : messagePrefix;
+
+      return action.then(function(data) {
+        return $q.resolve(data);
+      }).catch(function(error) {
+        return errorHandler(error, messagePrefix).then(function(message) {
+          if (!isPrivate) {
+            notify(true, {key:message});
+          }
+          return $q.reject(error);
+        });
+      });
+    }
+
   }
 })();
