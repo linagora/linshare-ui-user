70df341551898822f93d86903e69892bb1bc5124
Thang NGUYEN MANH <tnmanh@linagora.com>
2020-06-25 12:56:48 +0700

    #512: Improve UX on mobile by display number keyboard

---
+++ app/modules/linshare.secondFactorAuthentication/directives/secondFactorAuthenticationDirective.js
@@ -0,0 +1,25 @@
+/**
+ * secondFactorAuthenticationDirective Directive
+ * @namespace linshare.secondFactorAuthentication
+ */
+(function() {
+  'use strict';
+
+  angular
+    .module('linshare.secondFactorAuthentication')
+    .directive('onKeypress', function() {
+      return {
+          restrict: 'A',
+          link: function(scope, element) {
+            element.on('keypress', function(e){
+              var keyNumberConverted = Number(e.key);
+
+              if(isNaN(keyNumberConverted)){
+                e.preventDefault();
+              }
+            });
+          }
+      };
+    });
+
+})();
