d1c825c617fa639f4f871a86ce94c80e568ca98b
Bao Ngoc PHAM <bnpham@linagora.com>
2020-09-22 18:18:53 +0700

    Issue #559: Update translation

--- app/modules/linshare.uploadRequests/controllers/LinshareUploadRequestsController.js
+++ app/modules/linshare.uploadRequests/controllers/LinshareUploadRequestsController.js
@@ -171,29 +171,10 @@ function LinshareUploadRequestsController(
    *  @memberOf LinShare.UploadRequests.LinshareUploadRequestsController
    */
   function showDetails(uploadRequest = {}) {
-    uploadRequestRestService.get(uploadRequest.uuid).then((data) => {
-      uploadRequestVm.currentSelected = mapDataObject(data);
+    uploadRequestRestService.get(uploadRequest.uuid).then(data => {
+      uploadRequestVm.currentSelected = data;
       loadSidebarContent(uploadRequestVm.uploadRequestDetails, true);
     });
-  }
-
-  function mapDataObject(uploadRequest = {}) {
-    uploadRequest.maxTotalFileSize = formatBytes(uploadRequest.maxDepositSize);
-    uploadRequest.maxOneFileSize = formatBytes(uploadRequest.maxFileSize);
-
-    return uploadRequest;
-  }
-
-  function formatBytes(bytes, decimals = 2) {
-    if (bytes === 0) {return '0 Bytes';}
-  
-    const exchange = 1024;
-    const digits = decimals < 0 ? 0 : decimals;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-  
-    const unitIndex = Math.floor(Math.log(bytes) / Math.log(exchange));
-  
-    return parseFloat((bytes / Math.pow(exchange, unitIndex)).toFixed(digits)) + ' ' + sizes[unitIndex];
   }
 }
 
