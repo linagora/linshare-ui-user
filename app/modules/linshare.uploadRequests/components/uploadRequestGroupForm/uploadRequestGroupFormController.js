d22253cd4de60a564dc808ec699dd16fcbf0fc32
Bao Ngoc PHAM <bnpham@linagora.com>
2020-11-19 10:24:40 +0700

    Issue #673: Update datepicker validations

--- app/modules/linshare.uploadRequests/components/uploadRequestGroupForm/uploadRequestGroupFormController.js
+++ app/modules/linshare.uploadRequests/components/uploadRequestGroupForm/uploadRequestGroupFormController.js
@@ -2,12 +2,18 @@ angular
   .module('linshare.uploadRequests')
   .controller('uploadRequestGroupFormController', uploadRequestGroupFormController);
 
-function uploadRequestGroupFormController() {
+uploadRequestGroupFormController.$inject = ['moment'];
+function uploadRequestGroupFormController(moment) {
   const uploadRequestGroupFormVm = this;
 
   uploadRequestGroupFormVm.$onInit = onInit;
+  uploadRequestGroupFormVm.formatDateForDatepicker = formatDateForDatepicker;
 
   function onInit() {
     uploadRequestGroupFormVm.selectedTabIndex = uploadRequestGroupFormVm.selectedTabIndex || 0;
+  }
+
+  function formatDateForDatepicker(value, format = 'YYYY-MM-DD') {
+    return value ? moment(value).format(format) : value;
   }
 }
