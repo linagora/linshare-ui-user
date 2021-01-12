1a3957dce11ac52bc94864ddfe57238b47edf6aa
Dung Trung PHAM <dtpham@linagora.com>
2021-01-12 15:46:04 +0700

    Issue #805: Add a protectedByPassword for uploadRequest object

--- app/modules/linshare.uploadRequests/services/uploadRequestObjectCoreService.js
+++ app/modules/linshare.uploadRequests/services/uploadRequestObjectCoreService.js
@@ -63,6 +63,7 @@ function UploadRequestObjectCoreService(
       self.notificationDate = setPropertyValue(jsonObject.notificationDate, functionalityOfExpiryNotification.value);
       self.maxFileCount = setPropertyValue(jsonObject.maxFileCount, functionalityOfMaxNumberOfFiles.value);
       self.secured = setPropertyValue(jsonObject.secured, functionalityOfPasswordProtected.value);
+      self.protectedByPassword = setPropertyValue(jsonObject.protectedByPassword, functionalityOfPasswordProtected.value);
       self.canClose = setPropertyValue(jsonObject.canClose, functionalityOfClosure.value);
       self.canDelete = setPropertyValue(jsonObject.canDelete, functionalityOfDeletion.value);
       self.canDeleteDocument = setPropertyValue(jsonObject.canDeleteDocument, functionalityOfDeletion.value);
