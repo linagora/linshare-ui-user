4a7ab1d73f742ec71406c075846ee78f72e5d1af
Bao Ngoc PHAM <bnpham@linagora.com>
2021-02-25 14:04:23 +0700

    Issue #675: Automatically update hour when date change in upload request form

--- app/modules/linshare.uploadRequests/components/uploadRequestHourPicker/uploadRequestHourPickerController.js
+++ app/modules/linshare.uploadRequests/components/uploadRequestHourPicker/uploadRequestHourPickerController.js
@@ -2,17 +2,31 @@ angular
   .module('linshare.uploadRequests')
   .controller('uploadRequesetHourPickerController', uploadRequesetHourPickerController);
 
-uploadRequesetHourPickerController.$inject = ['moment'];
+uploadRequesetHourPickerController.$inject = ['moment', '$scope'];
 
-function uploadRequesetHourPickerController(moment) {
+function uploadRequesetHourPickerController(moment, $scope) {
   const hourPickerVm = this;
+  let unwatchDateTime;
 
   hourPickerVm.$onInit = $onInit;
+  hourPickerVm.$onDestroy = $onDestroy;
   hourPickerVm.changeHour = changeHour;
 
   function $onInit() {
     hourPickerVm.hour = moment(hourPickerVm.datetime).hour();
+
+    unwatchDateTime = $scope.$watch(() => hourPickerVm.datetime, (newValue, oldValue) => {
+      if (newValue && (!oldValue || (newValue.getTime() !== oldValue.getTime()))) {
+        changeHour();
+      }
+    });
   };
+
+  function $onDestroy() {
+    if (unwatchDateTime) {
+      unwatchDateTime();
+    }
+  }
 
   function changeHour() {
     hourPickerVm.datetime = moment(hourPickerVm.datetime).set({ hour: hourPickerVm.hour }).toDate();
