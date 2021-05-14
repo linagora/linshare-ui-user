3da3dfc3e2a4b20787a2ab89984184b27bf2caa9
Bao Ngoc PHAM <bnpham@linagora.com>
2021-05-14 15:44:35 +0700

    Issue #1009: Hide preview option for folders

--- app/modules/linshare.sharedSpace/controllers/WorkgroupNodesController.js
+++ app/modules/linshare.sharedSpace/controllers/WorkgroupNodesController.js
@@ -83,6 +83,7 @@ function WorkgroupNodesController(
   workgroupNodesVm.functionalities = {};
   workgroupNodesVm.canDeleteNodes = false;
   workgroupNodesVm.TYPE_DOCUMENT = TYPE_DOCUMENT;
+  workgroupNodesVm.TYPE_FOLDER = TYPE_FOLDER;
   workgroupNodesVm.permissions = workgroupPermissions;
   workgroupNodesVm.addUploadedDocument = addUploadedDocument;
   workgroupNodesVm.areAllSameType = areAllSameType;
