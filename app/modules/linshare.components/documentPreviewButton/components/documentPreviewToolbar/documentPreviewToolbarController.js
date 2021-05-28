20998c4df338363c2170c8ee919f1ce189f865aa
Dung Trung PHAM <dtpham@linagora.com>
2021-05-28 10:12:11 +0700

    Issue #1002: Hide download button in preview dialog when archive download is disabled

--- app/modules/linshare.components/documentPreviewButton/components/documentPreviewToolbar/documentPreviewToolbarController.js
+++ app/modules/linshare.components/documentPreviewButton/components/documentPreviewToolbar/documentPreviewToolbarController.js
@@ -2,68 +2,71 @@
  * DocumentPreviewToolbar Controller
  * @namespace linshare.components.documentPreviewButton.components
  */
-(function() {
-  'use strict';
 
-  angular
-    .module('linshare.components')
-    .controller('DocumentPreviewToolbarController', DocumentPreviewToolbarController);
+angular
+  .module('linshare.components')
+  .controller('DocumentPreviewToolbarController', DocumentPreviewToolbarController);
 
-  DocumentPreviewToolbarController.$inject = [
-    '_',
-    '$timeout',
-    'documentPreviewService',
-    'functionalityRestService'
-  ];
+DocumentPreviewToolbarController.$inject = [
+  '_',
+  'documentPreviewService',
+  'functionalityRestService'
+];
 
-  function DocumentPreviewToolbarController(
-    _,
-    $timeout,
-    documentPreviewService,
+function DocumentPreviewToolbarController(
+  _,
+  documentPreviewService,
+  functionalityRestService
+) {
+  const documentPreviewToolbarVm = this;
+
+  documentPreviewToolbarVm.$onInit = $onInit;
+  documentPreviewToolbarVm.$onChanges = $onChanges;
+  documentPreviewToolbarVm.close = documentPreviewService.close;
+  documentPreviewToolbarVm.copyToMySpace = documentPreviewService.copyToMySpace;
+  documentPreviewToolbarVm.copyToWorkgroup = documentPreviewService.copyToWorkgroup;
+  documentPreviewToolbarVm.download = documentPreviewService.download;
+  documentPreviewToolbarVm.executeAndClose = documentPreviewService.executeAndClose;
+  documentPreviewToolbarVm.showItemDetails = documentPreviewService.showItemDetails;
+  documentPreviewToolbarVm.canDownloadItem = canDownloadItem;
+  documentPreviewToolbarVm.TYPE_FOLDER = 'FOLDER';
+
+  ////////////
+
+  /**
+   * @name $onInit
+   * @desc Initialization function of the component
+   * @namespace linshare.components.documentPreviewButton.components.DocumentPreviewToolbarController
+   */
+  function $onInit() {
+    documentPreviewToolbarVm.canCopyToMySpace = !_.isNil(documentPreviewService.copyToMySpace);
+
+
     functionalityRestService
-  ) {
-    var documentPreviewToolbarVm = this;
-
-    documentPreviewToolbarVm.$onInit = $onInit;
-    documentPreviewToolbarVm.$onChanges = $onChanges;
-    documentPreviewToolbarVm.close = documentPreviewService.close;
-    documentPreviewToolbarVm.copyToMySpace = documentPreviewService.copyToMySpace;
-    documentPreviewToolbarVm.copyToWorkgroup = documentPreviewService.copyToWorkgroup;
-    documentPreviewToolbarVm.download = documentPreviewService.download;
-    documentPreviewToolbarVm.executeAndClose = documentPreviewService.executeAndClose;
-    documentPreviewToolbarVm.showItemDetails = documentPreviewService.showItemDetails;
-    documentPreviewToolbarVm.TYPE_FOLDER = 'FOLDER';
-
-    ////////////
-
-    /**
-     * @name $onInit
-     * @desc Initialization function of the component
-     * @namespace linshare.components.documentPreviewButton.components.DocumentPreviewToolbarController
-     */
-    function $onInit() {
-      documentPreviewToolbarVm.canCopyToMySpace = !_.isNil(documentPreviewService.copyToMySpace);
-
-      functionalityRestService.getFunctionalityParams('WORK_GROUP').then(functionality => {
-        if (functionality && functionality.enable) {
-          documentPreviewToolbarVm.canCopyToWorkGroup = !_.isNil(documentPreviewService.copyToWorkgroup);
-        } else {
-          documentPreviewToolbarVm.canCopyToWorkGroup = false;
-        }
+      .getAll()
+      .then(({
+        WORK_GROUP,
+        WORK_GROUP__DOWNLOAD_ARCHIVE
+      }) => {
+        documentPreviewToolbarVm.canCopyToWorkGroup = !_.isNil(documentPreviewService.copyToWorkgroup) && WORK_GROUP && WORK_GROUP.enable;
+        documentPreviewToolbarVm.downloadFolderEnabled = WORK_GROUP__DOWNLOAD_ARCHIVE && WORK_GROUP__DOWNLOAD_ARCHIVE.enable;
       });
-    }
-
-    /**
-     * @name $onChanges
-     * @desc Called whenever one-way bindings are updated
-     * @namespace linshare.components.documentPreviewButton.components.DocumentPreviewToolbarController
-     */
-    function $onChanges() {
-      documentPreviewToolbarVm.item = Object.assign(
-        {},
-        documentPreviewToolbarVm.item,
-        documentPreviewService.getItem()
-      );
-    }
   }
-})();
+
+  /**
+   * @name $onChanges
+   * @desc Called whenever one-way bindings are updated
+   * @namespace linshare.components.documentPreviewButton.components.DocumentPreviewToolbarController
+   */
+  function $onChanges() {
+    documentPreviewToolbarVm.item = Object.assign(
+      {},
+      documentPreviewToolbarVm.item,
+      documentPreviewService.getItem()
+    );
+  }
+
+  function canDownloadItem(item) {
+    return item.type === documentPreviewToolbarVm.TYPE_FOLDER ? documentPreviewToolbarVm.downloadFolderEnabled : true ;
+  }
+}
